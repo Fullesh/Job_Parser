a13a29ad383bca2409c0354abce304fbd8d4a93f
Danila Belov <danilabelov.adm@yandex.ru>
2024-01-18 18:19:26 +0300

    Added magic method __repr__ for class APIParser

--- api_parser/abstract_parser.py
+++ api_parser/abstract_parser.py
@@ -5,6 +5,10 @@ class APIparser(ABC):
     """
     Абстрактный класс для работы с API сайтов с вакансиями
     """
+
+    def __repr__(self):
+        return 'Абстрактный класс для парсинга вакансий. Использует метод get_vacancies'
+
     @abstractmethod
     def get_vacancies(self, request):
         """
